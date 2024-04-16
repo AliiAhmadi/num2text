0465a64f263dcc0fbf28966181010de68ab174b4
AliiAhmadi <aliahmadi.github@gmail.com>
2024-04-16 10:52:18 +0330

    + implement command line flag and main function for compile it to a binary

--- cli/main.go
+++ cli/main.go
@@ -5,10 +5,31 @@
 
 package main
 
-func flags() {
+import (
+	"flag"
+	"fmt"
+	"os"
 
+	"github.com/AliiAhmadi/num2text"
+)
+
+var (
+	INP string
+)
+
+func flags() {
+	flag.StringVar(&INP, "-num", "0", "number you want to convert")
+	flag.Parse()
 }
 
 func main() {
 	flags()
+
+	r, err := num2text.Convert(INP)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Result: %v", r)
 }
